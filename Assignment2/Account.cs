53acc7d6b01ef7af5b9ee4dbcf0a9fd9db11eed2
2284521 <2284521@D24318-FDCB3Q3>
2024-02-15 13:53:20 -0500

    Add project files.

---
+++ Assignment2/Account.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Account
+    {
+        private string accNumber;
+        private decimal accBalance;
+        private string lastName;
+        private string firstName;
+
+        public Account(string accNumber, decimal accBalance, string lastName, string firstName)
+        {
+            AccNumber = accNumber;
+           Balance = accBalance;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+        public string AccNumber
+        {
+            get
+            {
+                return this.accNumber;
+            }
+            set
+            {
+                this.accNumber = value;
+            }
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return this.accBalance;
+            }
+            set
+            {
+                if (accBalance >= 0.0m)
+                {
+                    this.accBalance = value;
+                } else
+                {
+                    this.accBalance = 0;
+                    Console.WriteLine("Account initial balance must be a positive value");
+                }
+            }
+
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+        public decimal credit(decimal amount)
+        {
+            return this.accBalance + amount;
+        }
+
+        public bool debit(decimal amount)
+        {
+            if (Balance < 0)
+            {
+                Console.WriteLine("Debit amount exceeded account balance");
+
+                return false;
+            }
+            Balance -= amount;
+            Console.WriteLine("Amount Withdrawn Successfully");
+            return true;
+
+        }
+
+
+        public void displayAccount()
+        {
+            drawLine();
+            Console.WriteLine($"|  {"ACCOUNT",45} {"|",33}");
+            drawLine();
+            Console.WriteLine($"| {"Account Number",-25} | {AccNumber,50} |");
+            drawLine();
+            Console.WriteLine($"| {"Balance Amount ",-25} | {"$" + Balance,50} |");
+            drawLine();
+            Console.WriteLine($"| {"Last Name",-25} | {LastName,-50} |");
+            drawLine();
+            Console.WriteLine($"| {"First Name",-25} | {FirstName,-50} |");
+            drawLine();
+        }
+
+
+        public static void drawLine()
+        {
+            Console.Write("|");
+            for (int i = 0; i < 80; i++)
+
+                Console.Write("-");
+            Console.WriteLine("|");
+
+        }
+    }
+}
+
+
---
+++ Assignment2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Account account = new Account("421",421.0m,"KM","Trevor");
+         //   account.displayAccount();
+           Console.WriteLine("Please Enter Account Number");
+            String accNumber = Console.ReadLine();
+            Console.WriteLine("Please Enter Account Balance");
+            decimal accBalance= decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Please Enter First Name");
+            string firstName = Console.ReadLine();
+            Account account2 = new Account(accNumber,accBalance,lastName,firstName);
+            account2.displayAccount();
+        }
+    }
+}
